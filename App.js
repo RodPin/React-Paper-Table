93bf1f983c6996dd3fe228c92a78fc6f864fc6cc
Rodrigo Pinto Coelho Gomes <41244788+RodPin@users.noreply.github.com>
2018-11-15 15:04:12 -0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+const oferta1 = {
+  cor: "branco",
+  ano: "2015",
+  fabricante: "Renatinho",
+  marca: "Ferrari",
+  seguro: "Golden Cross",
+  usuario: "rodd123@gmail.com"
+};
+
+const oferta2 = {
+  cor: "azul",
+  ano: "2015",
+  fabricante: "Renatinho",
+  marca: "Mercedes",
+  seguro: "sulamerica",
+  usuario: "renataotoaaot3@gmail.com"
+};
+const oferta3 = {
+  cor: "preto",
+  ano: "2013",
+  fabricante: "Bob",
+  marca: "Palho",
+  seguro: "StrawBerry",
+  usuario: "ricardo21123@gmail.com"
+};
+const oferta4 = {
+  cor: "amarelo",
+  ano: "2015",
+  fabricante: "Renatinho",
+  marca: "Lamborguini",
+  seguro: "watch",
+  usuario: "keaokeoao123@gmail.com"
+};
+
+const LISTADASOFERTAS = [oferta1, oferta2, oferta3, oferta4];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Pregao listaPregao={LISTADASOFERTAS}/>
+      </div>
+    );
+  }
+}
+
+
+export default App;
