c5f67014990c9fae8dbba1b7eee407810abe0e5c
Rodrigo Pinto Coelho Gomes <41244788+RodPin@users.noreply.github.com>
2018-11-15 15:08:42 -0200

    Update List.js

--- List.js
+++ List.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -7,15 +6,11 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import { auth } from "../../firebase/firebase";
-import CheckboxContainer from "../CheckBox";
 
 // https://github.com/mui-org/material-ui/blob/master/docs/src/pages/demos/tables/SimpleTable.js
 
 class List extends Component {
   componentWillMount() {
-    console.log("COMPONENTE - PREGAO");
-    console.log(this.props.listaPregao);
     this.renderNomes(this.props.listaPregao);
   }
   renderNomes(x) {
@@ -28,21 +23,9 @@ class List extends Component {
       for (var i = 0; i < x.length; i++) {
         data.push(Object.values(x[i]));
       }
-      console.log("--------DATA------------------");
-      console.log(data);
       this.setState({ data });
       this.setState({ listaPregao: this.props.listaPregao });
     }
-  }
-
-  mudarStatus(index) {
-    var IdEdicao = this.state.data[index].key;
-    firebase
-      .database()
-      .ref("Ofertas/" + IdEdicao)
-      .update({
-        status: "STATUS_DOWN"
-      });
   }
 
   render() {
@@ -76,29 +59,13 @@ class List extends Component {
                   return (
                     <TableRow>
                       {campos.map((values, i) => {
-                        if (values === "STATUS_UP") {
-                          if (
-                            this.state.data[index].data.usuario ==
-                            this.state.usuario
-                          ) {
                             return (
                               <TableCell>
-                                <button
-                                  onClick={() => {
-                                    this.mudarStatus(index);
-                                  }}
-                                >
-                                  Tirar
-                                </button>
+                                {values}
                               </TableCell>
                             );
-                          } else {
-                            return <TableCell> </TableCell>;
                           }
-                        } else {
-                          return <TableCell>{values}</TableCell>;
-                        }
-                      })}
+                      )}
                     </TableRow>
                   );
                 })}
